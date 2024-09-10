f701c061753e832fbf92e0838681b959db9149fa
i23332 <i23332@verisk.com>
2024-09-10 09:06:30 -0600

    execute title search, log response

--- src/app/api-helper.service.ts
+++ src/app/api-helper.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiHelperService {
 
-  constructor() { }
+  //add HttpClient to the constructor
+  constructor(private http: HttpClient) { }
+
+  getApiUrl(): string {
+    return 'https://openlibrary.org/search.json?q=';
+  }
+
+  getApiUrlWithQuery(query: string): string {
+    return this.getApiUrl() + query;
+  }
+
+  //search for a book based on the title using Angular HTTP GET
+  searchBook(title: string) {
+    return this.http.get(this.getApiUrlWithQuery(title));
+  }
+
 }
--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -5,6 +5,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {FormsModule} from "@angular/forms";
+import {ApiHelperService} from "../api-helper.service";
 
 @Component({
   selector: 'app-search-input',
@@ -21,6 +22,20 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './search-input.component.css'
 })
 export class SearchInputComponent {
+
+  //add ApiHelperService to the constructor
+  constructor(private apiHelperService: ApiHelperService) { }
+
   value: string | undefined;
+
+  //search for a book based on the title
+  search() {
+    if (!this.value)
+      return
+    this.apiHelperService.searchBook(this.value)
+      .subscribe((data: any) => {
+        console.log(data)
+      });
+  }
 
 }
