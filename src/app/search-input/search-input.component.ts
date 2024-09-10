30a459f29ca69dc16ad75fe8e512299cd8726f74
i23332 <i23332@verisk.com>
2024-09-10 08:42:02 -0600

    search form field

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule
+  ],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.css'
 })
 export class SearchInputComponent {
+  value: string | undefined;
 
 }
